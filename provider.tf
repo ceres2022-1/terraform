cbe6db6e0d3c1f4484de5559cba6f2e0edabfa56
Elizabeth Munoz <emunoz@tecnova.cl>
2023-01-24 18:50:20 -0300

    cada integrante debe utilizar su plataforma azure

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  subscription_id = "c60e8fe6-79d8-433f-889f-f81d4ace634f"
-  client_id       = "84ec65d6-60f6-41d2-86fc-fb27f0277495"
-  client_secret   = "RA98Q~d6HCfGvtQ~tZAm7vj2vXmkMm.JreimCayi"
-  tenant_id       = "c767a33a-c642-4323-bd8f-8ce4732773b7"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
   features {}
 }
